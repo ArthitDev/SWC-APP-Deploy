06f4c9ec84c36cf841b578781395e10ee920adad
Arthit LungYa <rthit096@gmail.com>
2024-08-10 23:51:26 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ const queryClient = new QueryClient();
 const prompt = Prompt({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
-  display: "swap",
+  display: 'swap',
 });
 
 type MyAppProps = AppProps & {
